fc74565519690869ef2405dcc0a27fa29adc6996
Derpy Moa <Johnny_me3010@hotmail.com>
2023-04-05 00:50:37 +0300

    V263

--- DerpyAero/Mods/Derpy's EE2 tweaks/Story/RawFiles/Lua/Ice&Fire.lua
+++ DerpyAero/Mods/Derpy's EE2 tweaks/Story/RawFiles/Lua/Ice&Fire.lua
@@ -3,7 +3,7 @@ local function DerpyTalentTooltipOverride(character, talent, tooltip)
     if talent == "IceKing" then
         description.Description = "Once per cast, applying Brittle to enemies will deal water damage equal to 50% of its detonation damage. Your Brittle status requires 1 less stack of Battered to detonate. Detonating Brittle lowers the cooldown of Glaciate and Shatter by 1 turn. Your own Shatter does not damage you anymore."
     elseif talent == "Demon" then
-        description.Description = "Once per cast, applying Scorched to enemies will deal fire damage equal to 50% of its detonation damage. Your Scorched status also deals its damage at the start of its target's turn. Once per round while Scorched, your next basic attack or weapon skill (separate ICDs) will remove it to deal 125% Fire damage in a 3m radius to enemies on impact. Fire surfaces (except Lava) do not damage you anymore. If the target enemy is at or over 100% Fire resistance, deal Physical damage instead of Fire damage."
+        description.Description = "Once per cast, applying Scorched to enemies will deal fire damage equal to 50% of its detonation damage. Your Scorched status also deals its damage at the start of its target's turn. Once per round while Scorched, your next basic attack or weapon skill (separate ICDs) will remove it to deal 50% (+5% per level; max 150%) Fire damage in a 3m radius to enemies on impact. Fire surfaces (except Lava) do not damage you anymore. If the target enemy is at or over 100% Fire resistance, deal Physical damage instead of Fire damage."
     end
 end
 
