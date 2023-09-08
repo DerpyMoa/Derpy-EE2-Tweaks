505ff15da8a1156fc349e684c51c6ddf8753f884
Derpy Moa <Johnny_me3010@hotmail.com>
2023-09-08 23:16:44 +0300

    Small fix

--- DerpyAero/Mods/Derpy's EE2 tweaks/Story/RawFiles/Lua/BootstrapClient.lua
+++ DerpyAero/Mods/Derpy's EE2 tweaks/Story/RawFiles/Lua/BootstrapClient.lua
@@ -1,5 +1,8 @@
-
-Mods.EpipEncounters.Epip.ImportGlobals(_ENV)
+if Mods.EpipEncounters.Epip.ImportGlobals then
+    Mods.EpipEncounters.Epip.ImportGlobals(_ENV)
+else
+    _ENV = Mods.EpipEncounters
+end
 
 Ext.Require("Derpy's EE2 tweaks_0701303b-719f-40e6-b554-3f6515b08268", "SkillTextPatching.lua");
 Ext.Require("Derpy's EE2 tweaks_0701303b-719f-40e6-b554-3f6515b08268", "SkillTextPatchingPartTwo.lua");
